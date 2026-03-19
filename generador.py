00d0781f146e4f26fe6fe7b53717b082c55545be
Alyson Sofia Prato Baron <aprato@acueducto.com.co>
2026-03-19 16:39:28 -0500

    no se cambian los llamados en word

--- generador.py
+++ generador.py
@@ -5,19 +5,19 @@ from docxtpl import DocxTemplate
 
 # FUNCIONES
 
-# def limpiar_texto(valor):
-#     if pd.isna(valor):
-#         return ""
-#     return str(valor).strip()
+def limpiar_texto(valor):
+    if pd.isna(valor):
+        return ""
+    return str(valor).strip()
 
-# def normalizar(texto):
-#     return limpiar_texto(texto).lower()
+def normalizar(texto):
+    return limpiar_texto(texto).lower()
 
-# def limpiar_nombre_archivo(valor):
-#     texto = limpiar_texto(valor)
-#     texto = re.sub(r'[\\/*?:"<>|]', "", texto)
-#     texto = texto.replace("  ", " ").strip()
-#     return texto
+def limpiar_nombre_archivo(valor):
+    texto = limpiar_texto(valor)
+    texto = re.sub(r'[\\/*?:"<>|]', "", texto)
+    texto = texto.replace("  ", " ").strip()
+    return texto
 
 # LEER EXCEL
 
@@ -50,24 +50,31 @@ for p in plantillas:
 # RELACIONA DESCRIPCION PLANTILLA
 
 mapa_descripciones = {
-
-    "taponamiento del servicio": "Taponamiento Efectivo.docx",
-
-    "revision interna": "Solo Datos.docx",
-    "revisiones internas": "Solo Datos.docx",
-    "revision interna con geofono": "Solo Datos.docx",
-    "revisiones internas//llamar antes de ir": "Solo Datos.docx",
-
-    "vinculacion": "vincu inefectiva sin putos hidraulicos.docx",
-    "vinculación inefectiva": "vincu inefectiva sin putos hidraulicos.docx",
-
-    "suspension temporal del servicio": "Suspension efectiva.docx",
-
-    "solicitud lnivelacion cajilla": "Solo Datos.docx",
-
-    "unidades habitacionales": "Solo Datos.docx"
+    "taponamiento efectivo": "Taponamiento Efectivo.docx",
+    "taponamiento inefectivo": "Taponamiento Inefectivo Deuda.docx",
+    "activar factura virtual": "ACTIVAR FACTURA VIRTUAL (1).docx",
+    "desactivar factura virtual": "Desactivar factura virtual.docx",
+    "cambio de nombre efectivo" : "cambio de nombre efectivo.docx",
+    "cambio de nombre inefectivo": "cambio de nombre inefectivo.docx",
+    "independdizacion inefectiva" : "independizacion inefectiva deuda.docx",
+    "independizacon efectiva" : "NUEVA ACOMETIDA EFECTIVA ESPERA.docx",
+    "nueva acometida inefectiva" : "nueva acometida inefectiva documentos.docx",
+    "nueva acometida efectiva" : "NUEVA ACOMETIDA EFECTIVA ESPERA.docx",
+    "visita": "Informacion visita (1).docx",
+    "normalizacion efectivaa" : "Normalizacion Proximo A Ejecutar.docx",
+    "Paz y salvo efectivo": "Paz y salvo efectivo.docx",
+    "Paz y salvo efectivo": "Paz y salvo.docx",
+    "Paz y salvo inefectivo" : "Paz y salvo inefectivo.docx",
+    "reconexion efectiva" : "reconexion efectiva.docx",
+    "reconexion inefectiva" : "Reconexion inefectiva Deuda.docx",
+    "Revision con geofono efectiva" : "REVISION CON GEOFONO EFECTIVA (1).docx",
+    "Revision con geofono inefectiva" : "REVISION CON GEOFONO INEFECTIVA (1).docx",
+    "suspension efectiva" : "Suspension efectiva.docx",
+    "suspension inefectiva" : "Suspension inefectiva deuda.docx",
+    "vinculacion inefectiva" : "vincu inefectiva sin putos hidraulicos.docx",
 }
 
+  
 
 # RENOMBRE COLUMNAS
 
